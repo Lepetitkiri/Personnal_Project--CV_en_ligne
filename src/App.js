4c9f4080d0cd27ed31964b6f6a1f03b12687e423
Domitille REGNAULT <domitille.regnault@hotmail.fr>
2024-03-17 14:40:28 +0100

    Navigation bug fixed

--- src/App.js
+++ src/App.js
@@ -14,17 +14,18 @@ function App() {
   const basename = "/Personnal_Project--CV_en_ligne";
 
   return (
-    <BrowserRouter basename="/Personnal_Project--CV_en_ligne" >
+    <BrowserRouter basename={basename}>
       <Routes>
-        <Route path="*" element={<Error basename={basename} />} />
         <Route path="/" element={<Home basename={basename} />} />
         <Route path="/Error" element={<Error basename={basename} />} />
         <Route path="/Projects" element={<Projects basename={basename} />} />
         <Route path="/CV" element={<CV basename={basename} />} />
         <Route path="/Contact" element={<Contact basename={basename} />} />
+        <Route path="*" element={<Error basename={basename} />} />
       </Routes>
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
+
 
 export default App;
